a46426e32b5410d5cfad626e2527259a8e9c0adb
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-06-07 23:57:18 +0900

    Flash tile, collapse tile image applied. dash spectrum effect added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,9 +9,12 @@ public class Player : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
 
     [SerializeField] private float _jumpForce, _moveSpeed, _dashForce, _glidDrag, _dashTime;
+    [SerializeField] private SpriteRenderer _playerSpectrum;
     private int _maxJumpCount = 2, _jumpCount = 2;
     private bool _isOnGround = false;
     private float _dashTimer = 0f, _dashDir;
+    private float _dashSpectrumTimer = 0f;
+    private int _spectrumCount = 0;
     private float _originalDrag;
 
     private void Awake()
@@ -53,7 +56,37 @@ public class Player : MonoBehaviour
         {
             if (_dashDir * hor < 0) _dashTimer = 0f;
             _rigid.velocity = Vector2.right * _dashDir;
+
+            if((_dashSpectrumTimer -= Time.deltaTime) < 0f)
+            {
+                _dashSpectrumTimer += _dashTime / 4f;
+                var spectrum = Instantiate(_playerSpectrum, transform.position, Quaternion.identity);
+                spectrum.sortingOrder -= _spectrumCount;
+                var col = spectrum.color;
+                col.a = 0.2f + _spectrumCount * 0.2f;
+                spectrum.color = col;
+                StartCoroutine(SpectrumRoutine(spectrum));
+                _spectrumCount++;
+            }
         }
+        else if(_spectrumCount > 0)
+        {
+            _spectrumCount = 0;
+            _dashSpectrumTimer = 0f;
+        }
+    }
+
+    private IEnumerator SpectrumRoutine(SpriteRenderer spriteRenderer)
+    {
+        var col = spriteRenderer.color;
+        var originalAlpha = col.a;
+        for(var i = 1f; i > 0f; i -= Time.deltaTime)
+        {
+            col.a = originalAlpha * i;
+            spriteRenderer.color = col;
+            yield return null;
+        }
+        Destroy(spriteRenderer.gameObject);
     }
 
     public void Jump(float force)
