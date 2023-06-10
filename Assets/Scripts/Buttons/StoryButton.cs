7bc478d1b204cc2bde47739c1810019d0ab83788
MunjiRules <aprobuldok0825@naver.com>
2023-06-10 23:34:42 +0900

    Story Check + Stage Move
    Story Check + Stage Move

--- Assets/Scripts/Buttons/StoryButton.cs
+++ Assets/Scripts/Buttons/StoryButton.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class StoryButton : CanvasOnOff
 {
-    [SerializeField] int _storyNum;
+    [SerializeField] public int _storyNum;
+    [SerializeField] public Text _warnText;
     Image _storyImage;
     Sprite _sprite;
 
@@ -20,15 +21,47 @@ public class StoryButton : CanvasOnOff
     {
         base.Start();
         _button.onClick.AddListener(OnClickStoryButton);
+        _warnText.text = "";
 
         if (PlayerPrefs.GetInt("CollectedPictureCount" + _storyNum) != 3)
         {
+            //컷씬 잠금해제 안됨
             _button.onClick.RemoveAllListeners();
+            _button.onClick.AddListener(OnClickStoryLocked);
         }
     }
 
     public void OnClickStoryButton()
     {
+
         _storyImage.sprite = _sprite;
+    }
+
+    public void OnClickStoryLocked()
+    {
+        _warnText.text = "사진 조각이 부족합니다.";
+        WarnText();
+    }
+
+    public void WarnText()
+    {
+        _warnText.color = new Color(_warnText.color.r, _warnText.color.b, _warnText.color.g, 1);
+        StartCoroutine(StoryLockedText());
+    }
+
+    IEnumerator StoryLockedText()
+    {
+        yield return new WaitForFixedUpdate();
+
+        _warnText.color = new Color(_warnText.color.r, _warnText.color.b, _warnText.color.g, _warnText.color.a - Time.deltaTime);
+
+        if (_warnText.color.a <= 0)
+        {
+            yield return null;
+        }
+        else
+        {
+            StartCoroutine(StoryLockedText());
+        }
     }
 }
