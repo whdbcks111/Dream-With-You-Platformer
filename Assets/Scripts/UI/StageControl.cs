4d5f93e43e89950204cf1909c8accb5b0751fced
MunjiRules <aprobuldok0825@naver.com>
2023-06-12 22:40:24 +0900

    Stage 2

---
+++ Assets/Scripts/UI/StageControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageControl : MonoBehaviour
+{
+    [SerializeField] Text _text;
+
+    int _currentStage;
+
+    void Awake()
+    {
+        _currentStage = PlayerPrefs.GetInt("EnterStage");
+        _text.text = "Stage " + _currentStage;
+        Instantiate(Resources.Load("StagePrefabs/Stage" + _currentStage));   
+    }
+}
