8837aa2bd39a24cff7b43645d35cc9f28657092e
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-06-12 12:08:08 +0900

    Background sprite fit script added

---
+++ Assets/Scripts/Utils/BackgroundFit.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BackgroundFit : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        Fit();
+    }
+
+    private void Fit()
+    {
+        var leftBottom = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        var rightTop = Camera.main.ViewportToWorldPoint(Vector2.one);
+
+        var boundsSize = _spriteRenderer.bounds.size;
+        var spriteRatio = boundsSize.x / boundsSize.y;
+        var screenRatio = (rightTop.x - leftBottom.x) / (rightTop.y - leftBottom.y);
+
+        print(boundsSize);
+        print(leftBottom);
+        print(rightTop);
+
+        _spriteRenderer.gameObject.transform.position = ((rightTop + leftBottom) / 2f) * Vector2.one;
+
+        if(screenRatio > spriteRatio)
+        {
+            _spriteRenderer.gameObject.transform.localScale *= ((rightTop.x - leftBottom.x) / boundsSize.x);
+        }
+        else
+        {
+            _spriteRenderer.gameObject.transform.localScale *= ((rightTop.y - leftBottom.y) / boundsSize.y);
+        }
+    }
+}
